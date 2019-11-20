3b129adfc94047104718a2cbfe53d141697579f3
daskaloff <daskalovia@gmail.com>
2019-11-20 15:35:27 +0100

    Initial Commit

---
+++ AperoApp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using AperoApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AperoApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
